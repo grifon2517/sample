4c9a67bec229ee6a5a1a88335e68bf080ffe5c83
Gregory Chebatyuk <gchebatyu@gmail.com>
2024-11-15 14:18:26 +0300

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import styles from './app.module.css';
+import PropTypes from 'prop-types';
+
+const AppLayout = ({ a, b, sum, setA, setB }) => (
+	<div className={styles.App}>
+		<div> A: {a}</div>
+		<button onClick={() => setA(a + 1)}>прибавить 1</button>
+		<div> B: {b}</div>
+		<button onClick={() => setB(b + 1)}>прибавить 1</button>
+		<div>Сумма A+B: {sum}</div>
+	</div>
+);
 
 export const App = () => {
-	const [count, setCount] = useState(0);
+	const [a, setA] = useState(0);
+	const [b, setB] = useState(0);
 
-	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite2024 + React2024</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
-	);
+	const sum = a + b;
+	console.log(sum);
+	return <AppLayout a={a} b={b} sum={sum} setA={setA} setB={setB} />;
+};
+
+AppLayout.propTypes = {
+	a: PropTypes.number.isRequired,
+	b: PropTypes.number.isRequired,
+	sum: PropTypes.number.isRequired,
+	setA: PropTypes.func.isRequired,
+	setB: PropTypes.func.isRequired,
 };
